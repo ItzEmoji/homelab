f44538ca38a321e0cfff1fbc2261d3e71757260e
ItzEmoji <no-reply@itzemoji.com>
2025-12-24 11:49:16 +0000

    updated flake.lock

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,9 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
+    ./minecraft.nix
   ];
+  nixpkgs.config.allowUnfree = true;
   boot.loader.grub = {
     efiSupport = true;
     efiInstallAsRemovable = true;
