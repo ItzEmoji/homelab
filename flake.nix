67ca4b0ca0ced71f64f1b77038bb4826e693703a
ItzEmoji <cyril.dettling@gmail.com>
2025-12-27 19:04:42 +0000

    added minecraft-nix and added a whitelist.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { nixpkgs, disko, ...}:
+  outputs = { nixpkgs, disko, ...}@inputs:
     {
       nixosConfigurations.server = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
           disko.nixosModules.disko
           ./configuration.nix
--- minecraft.nix
+++ minecraft.nix
@@ -1,11 +1,20 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 {
+  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
   services.minecraft-server = {
     enable = true;
     eula = true;
     declarative = true;
+    package = pkgs.vanillaServers.vanilla-1_21_11;
+    dataDir = "/srv/minecraft-server/survival-1.21.10";
     serverProperties = {
-      keepInventory = true;
+      white-list = true;
+    };
+    whitelist = 
+    {
+        ItzEmoji = "9764484a-7e56-4b31-8bb1-91d0f535292a";
+        Sel_X = "34a3195a-88cf-4da3-bd1c-7b71ed4eb4f6";
+        Aleksus401 = "ad541490-a394-4474-83b9-6fee5ae50aef";
     };
   };
 }
