19b9e8d6417d1391f0bc733411c7d258c67bdd78
ItzEmoji <cyril.dettling@gmail.com>
2026-01-09 17:32:04 +0000

    added oneblock server and migrated completly to nix-minecraft.

--- configuration.nix
+++ configuration.nix
@@ -16,9 +16,16 @@
   environment.systemPackages = with pkgs; [
     fastfetch
     neovim
+    tmux
     vim
     docker-compose
     curl
+    cacert
+    maven
+    git
+    temurin-bin-21
+    k9s
+    dive
     gitMinimal
     wget
   ];
